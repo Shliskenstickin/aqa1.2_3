8143dd78767c0c2e7fcfe3b12c7da58612e4ba0f
Shliskenstickin <deathwalker@mail.ru>
2021-05-11 16:50:55 +0300

    test fix2

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,6 +19,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("text/plain; charset=UTF-8"));
+                .body("data", equalTo("/Привет/тут?рука=волна"));
     }
 }
